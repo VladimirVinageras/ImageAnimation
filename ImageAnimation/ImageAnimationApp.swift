e813786051f7c8f74f03d0dab8e2d6c79c07e6ba
Vladimir Vinakheras <vvinageras@gmail.com>
2024-01-24 17:41:08 +0200

    Initial Commit

---
+++ ImageAnimation/ImageAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageAnimationApp.swift
+//  ImageAnimation
+//
+//  Created by Vladimir Vinakheras on 24.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct ImageAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
